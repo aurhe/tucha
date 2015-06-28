73d6211570e17eccef084394846f834ca87de47b
aurhe <aurhes@gmail.com>
2015-06-28 23:48:10 +0100

    allow new animals to be added

--- server/main.js
+++ server/main.js
@@ -22,7 +22,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: '127.0.0.1',
     port: '3306',
-    user: 'root'
+    user: 'root',
+    password: 'mysql'
 });
 
 connection.connect();
@@ -31,30 +32,57 @@ connection.connect();
 app.use(express.static('public'));
 
 //rest
-app.get('/r/animals', function(req, res) {
-    connection.query('select * from tucha.animal', function(err, rows) {
+app.get('/r/animals', function (req, res) {
+    console.log('select * from tucha.animal');
+    connection.query('select * from tucha.animal', function (err, rows) {
         res.json(rows);
     });
 });
 
-app.get('/r/animal/:id', function(req, res) {
-    connection.query('select * from tucha.animal where id=' + req.params.id, function(err, rows) {
+app.get('/r/animal/:id', urlencodedParser, function (req, res) {
+    console.log('select * from tucha.animal where id=' + req.params.id);
+    connection.query('select * from tucha.animal where id=' + mysql.escape(req.params.id), function (err, rows) {
         res.json(rows[0]);
     });
 });
 
-app.post('/r/animal/:id', urlencodedParser, function(req, res) {
-    var sql = 'update tucha.animal set ',
+app.post('/r/animal/:id', urlencodedParser, function (req, res) {
+    var sql, keys;
+    if (req.params.id === 'new') {
+        sql = 'insert into tucha.animal (';
         keys = Object.keys(req.body);
 
-    for (var i = keys.length - 1; i > 0; i--) {
-        sql += keys[i] + '=\'' + req.body[keys[i]] + '\', ';
-    }
-    sql += keys[0] + '=\'' + req.body[keys[0]] + '\' where id=' + req.params.id;
+        for (var i = 0; i < keys.length - 1; i++) {
+            sql += keys[i] + ', '
+        }
+        sql += keys[keys.length - 1] + ') values (';
+        for (var i = 0; i < keys.length - 1; i++) {
+            sql += mysql.escape(req.body[keys[i]]) + ', '
+        }
+        sql += mysql.escape(req.body[keys[keys.length - 1]]) + ')';
+        console.log(sql);
+        connection.query(sql, function (err, rows) {
+            if (err) {
+                throw err;
+            }
+            res.redirect('/#/animals');
+        });
+    } else {
+        sql = 'update tucha.animal set ';
+        keys = Object.keys(req.body);
 
-    connection.query(sql, function(err, rows) {
-        res.redirect('/#/animals');
-    });
+        for (var i = 0; i < keys.length - 1; i++) {
+            sql += keys[i] + '=' + mysql.escape(req.body[keys[i]]) + ', ';
+        }
+        sql += keys[keys.length - 1] + '=' + mysql.escape(req.body[keys[keys.length - 1]]) + ' where id=' + mysql.escape(req.params.id);
+        console.log(sql);
+        connection.query(sql, function (err, rows) {
+            if (err) {
+                throw err;
+            }
+            res.redirect('/#/animals');
+        });
+    }
 });
 
 // app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
