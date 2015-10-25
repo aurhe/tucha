9414806d166856491756fb53ab1785bd231f42e9
Aurelio Santos <aurhes@gmail.com>
2015-10-25 20:54:23 +0000

    fix column values

--- server/main.js
+++ server/main.js
@@ -150,20 +150,21 @@ function storeImage(id, buffer, callback) {
 
 function saveState(state) {
     if (state.deleted) {
-        connection.query('update tucha.state set is_deleted=true where position=' + state.position + ' and animal=' + state.animal);
+        connection.query('update tucha.state set is_deleted=true where position=' + state.position +
+            ' and animal=' + state.animal + ' and is_deleted is null');
     } else {
-        connection.query('select * from tucha.state where position=' + state.position + ' and animal=' + state.animal,
-            function (err, rows) {
-                var sql;
+        connection.query('select * from tucha.state where position=' + state.position + ' and animal=' + state.animal +
+            ' and is_deleted is null', function (err, rows) {
+            var sql;
 
-                if (err || rows.length === 0) {
-                    sql = 'insert into tucha.state set ?';
-                } else if (rows.length > 0) {
-                    sql = 'update tucha.state set ? where position=' + state.position + ' and animal=' + state.animal;
-                }
+            if (err || rows.length === 0) {
+                sql = 'insert into tucha.state set ?';
+            } else if (rows.length > 0) {
+                sql = 'update tucha.state set ? where position=' + state.position + ' and animal=' + state.animal;
+            }
 
-                connection.query(sql, state, logQueryError);
-            });
+            connection.query(sql, state, logQueryError);
+        });
     }
 }
 
@@ -175,6 +176,15 @@ var selects = {
         ' chip_code, is_sterilizated, sterilization_date, sterilization_by, sterilization_details,' +
         ' current_situation, missing_details, death_date, death_reason' +
         ' from tucha.animal where is_deleted is null',
+        grid: 'select a.id, a.code, a.name, a.species, a.gender, a.breed, a.date_of_birth, a.size, a.color,' +
+        ' a.details, a.is_adoptable, a.is_adoptable_reason, p1.name as received_by, p2.name as received_from, a.received_date,' +
+        ' a.chip_code, a.is_sterilizated, a.sterilization_date, v.name as sterilization_by, a.sterilization_details,' +
+        ' a.current_situation, a.missing_details, a.death_date, a.death_reason' +
+        ' from tucha.animal a ' +
+        ' left join tucha.person p1 on p1.id = a.received_by' +
+        ' left join tucha.person p2 on p2.id = a.received_from' +
+        ' left join tucha.veterinary v on v.id = a.sterilization_by' +
+        ' where a.is_deleted is null',
         dropdown: 'select id, name from tucha.animal where is_deleted is null',
         adoptableAnimals: 'select id,name,gender from tucha.animal where is_adoptable=true and ' +
         '(current_situation="IN_SHELTER" or current_situation="FAT" or current_situation="FAR") and ' +
@@ -193,39 +203,67 @@ var selects = {
     },
     user: {
         entity: 'user',
-        details: 'select username, role, person from tucha.user where is_deleted is null'
+        details: 'select username, role, person from tucha.user where is_deleted is null',
+        grid: 'select u.username, u.role, p.name as person from tucha.user u' +
+        ' left join tucha.person p on p.id = u.person where u.is_deleted is null'
     },
     medicalExam: {
         entity: 'medical_exam',
-        details: 'select id, animal, date, veterinary, details from tucha.medical_exam where is_deleted is null'
+        details: 'select id, animal, date, veterinary, details from tucha.medical_exam where is_deleted is null',
+        grid: 'select m.id, a.name as animal, m.date, v.name as veterinary, m.details from tucha.medical_exam m' +
+        ' left join tucha.animal a on a.id = m.animal' +
+        ' left join tucha.veterinary v on v.id = m.veterinary where m.is_deleted is null'
     },
     vaccination: {
         entity: 'vaccination',
-        details: 'select id, animal, date, veterinary, details from tucha.vaccination where is_deleted is null'
+        details: 'select id, animal, date, veterinary, details from tucha.vaccination where is_deleted is null',
+        grid: 'select va.id, a.name as animal, va.date, ve.name as veterinary, va.details from tucha.vaccination va' +
+        ' left join tucha.animal a on a.id = va.animal' +
+        ' left join tucha.veterinary ve on ve.id = va.veterinary where va.is_deleted is null'
     },
     deparasitation: {
         entity: 'deparasitation',
-        details: 'select id, animal, date, veterinary, details from tucha.deparasitation where is_deleted is null'
+        details: 'select id, animal, date, veterinary, details from tucha.deparasitation where is_deleted is null',
+        grid: 'select d.id, a.name as animal, d.date, v.name as veterinary, d.details from tucha.deparasitation d' +
+        ' left join tucha.animal a on a.id = d.animal' +
+        ' left join tucha.veterinary v on v.id = d.veterinary where d.is_deleted is null'
     },
     medicalTreatment: {
         entity: 'medical_treatment',
-        details: 'select id, animal, date, veterinary, details from tucha.medical_treatment where is_deleted is null'
+        details: 'select id, animal, date, veterinary, details from tucha.medical_treatment where is_deleted is null',
+        grid: 'select m.id, a.name as animal, m.date, v.name as veterinary, m.details from tucha.medical_treatment m' +
+        ' left join tucha.animal a on a.id = m.animal' +
+        ' left join tucha.veterinary v on v.id = m.veterinary where m.is_deleted is null'
     },
     medicamentPrescription: {
         entity: 'medicament_prescription',
-        details: 'select id, animal, date, veterinary, details from tucha.medicament_prescription where is_deleted is null'
+        details: 'select id, animal, date, veterinary, details from tucha.medicament_prescription where is_deleted is null',
+        grid: 'select m.id, a.name as animal, m.date, v.name as veterinary, m.details from tucha.medicament_prescription m' +
+        ' left join tucha.animal a on a.id = m.animal' +
+        ' left join tucha.veterinary v on v.id = m.veterinary where m.is_deleted is null',
+        dropdown: 'select id, details as name from tucha.medicament_prescription where is_deleted is null'
     },
     aggressivityReport: {
         entity: 'aggressivity_report',
-        details: 'select id, animal, date, reported_by, details from tucha.aggressivity_report where is_deleted is null'
+        details: 'select id, animal, date, reported_by, details from tucha.aggressivity_report where is_deleted is null',
+        grid: 'select ag.id, an.name as animal, ag.date, p.name as reported_by, ag.details' +
+        ' from tucha.aggressivity_report ag' +
+        ' left join tucha.animal an on an.id = ag.animal' +
+        ' left join tucha.person p on p.id = ag.reported_by where ag.is_deleted is null'
     },
     escapeReport: {
         entity: 'escape_report',
-        details: 'select id, animal, date, details, returned_date from tucha.escape_report where is_deleted is null'
+        details: 'select id, animal, date, details, returned_date from tucha.escape_report where is_deleted is null',
+        grid: 'select e.id, a.name as animal, e.date, e.details, e.returned_date from tucha.escape_report e' +
+        ' left join tucha.animal a on a.id = e.animal where e.is_deleted is null'
     },
     adoption: {
         entity: 'adoption',
-        details: 'select id, animal, date, details, adoptant from tucha.adoption where is_deleted is null'
+        details: 'select id, animal, date, details, adoptant from tucha.adoption where is_deleted is null',
+        grid: 'select ad.id, an.name as animal, ad.date, ad.details, p.name as adoptant from tucha.adoption ad' +
+        ' left join tucha.animal an on an.id = ad.animal' +
+        ' left join tucha.person p on p.id = ad.adoptant' +
+        ' where ad.is_deleted is null'
     },
     medicament: {
         entity: 'medicament',
@@ -239,39 +277,74 @@ var selects = {
     },
     donation: {
         entity: 'donation',
-        details: 'select id, details, donated_by from tucha.donation where is_deleted is null'
+        details: 'select id, details, donated_by from tucha.donation where is_deleted is null',
+        grid: 'select d.id, d.details, p.name as donated_by from tucha.donation d' +
+        ' left join tucha.person p on p.id = d.donated_by where d.is_deleted is null'
     },
     medicamentUnit: {
         entity: 'medicament_unit ',
-        details: 'select id, medicament, details, used, used_in, opening_date, expiration_date, bought_in, ' +
+        details: 'select id, medicament, details, used, opening_date, expiration_date, bought_in, ' +
         'bought_by, donated_by, acquired_date, unitary_price, initial_quantity, remaining_quantity ' +
-        'from tucha.medicament_unit where is_deleted is null'
+        'from tucha.medicament_unit where is_deleted is null',
+        grid: 'select mu.id, me.name as medicament, mu.details, mu.used, mu.opening_date,' +
+        ' mu.expiration_date, s.name as bought_in, p1.name as bought_by, p2.name as donated_by, mu.acquired_date,' +
+        ' mu.unitary_price, mu.initial_quantity, mu.remaining_quantity' +
+        ' from tucha.medicament_unit mu' +
+        ' left join tucha.medicament me on me.id = mu.medicament' +
+        ' left join tucha.supplier s on s.id = mu.bought_in' +
+        ' left join tucha.person p1 on p1.id = mu.bought_by' +
+        ' left join tucha.person p2 on p2.id = mu.donated_by' +
+        ' where mu.is_deleted is null'
     },
     devolution: {
         entity: 'devolution',
-        details: 'select id, animal, adoptant, reason, date from tucha.devolution where is_deleted is null'
+        details: 'select id, animal, adoptant, reason, date from tucha.devolution where is_deleted is null',
+        grid: 'select d.id, a.name as animal, p.name as adoptant, d.reason, d.date from tucha.devolution d' +
+        ' left join tucha.person p on p.id = d.adoptant' +
+        ' left join tucha.animal a on a.id = d.animal where d.is_deleted is null'
     },
     medicamentUsed: {
         entity: 'medicament_used',
         details: 'select id, medicament, animal, administrator, prescription, date, quantity ' +
-        'from tucha.medicament_used where is_deleted is null'
+        'from tucha.medicament_used where is_deleted is null',
+        grid: 'select mu.id, me.name as medicament, a.name as animal, p.name as administrator,' +
+        ' mp.details as prescription, mu.date, mu.quantity' +
+        ' from tucha.medicament_used mu' +
+        ' left join tucha.medicament me on me.id = mu.medicament' +
+        ' left join tucha.animal a on a.id = mu.animal' +
+        ' left join tucha.person p on p.id = mu.administrator' +
+        ' left join tucha.medicament_prescription mp on mp.id = mu.prescription' +
+        ' where mu.is_deleted is null'
     },
     medicamentSupplier: {
         entity: 'medicament_supplier',
-        details: 'select supplier, medicament from tucha.medicament_supplier where is_deleted is null'
+        details: 'select supplier, medicament from tucha.medicament_supplier where is_deleted is null',
+        grid: 'select s.name as supplier, me.name as medicament from tucha.medicament_supplier ms' +
+        ' left join tucha.supplier s on s.id = ms.supplier' +
+        ' left join tucha.medicament me on me.id = ms.medicament' +
+        ' where ms.is_deleted is null'
     },
     volunteer: {
         entity: 'volunteer',
         details: 'select id, person, disponibility, activities, expertises, connections ' +
-        'from tucha.volunteer where is_deleted is null'
+        'from tucha.volunteer where is_deleted is null',
+        grid: 'select v.id, p.name as person, v.disponibility, v.activities, v.expertises, v.connections ' +
+        'from tucha.volunteer v left join tucha.person p on p.id = v.person where v.is_deleted is null'
     },
     host: {
         entity: 'host',
-        details: 'select id, animal, person, start_date, end_date, details from tucha.host where is_deleted is null'
+        details: 'select id, animal, person, start_date, end_date, details from tucha.host where is_deleted is null',
+        grid: 'select h.id, a.name as animal, p.name as person, h.start_date, h.end_date, h.details' +
+        ' from tucha.host h' +
+        ' left join tucha.animal a on a.id = h.animal' +
+        ' left join tucha.person p on p.id = h.person' +
+        ' where h.is_deleted is null'
     },
     state: {
         entity: 'state',
-        details: 'select id, animal, date, details, position from tucha.state where is_deleted is null'
+        details: 'select id, animal, date, details, position from tucha.state where is_deleted is null',
+        grid: 'select s.id, a.name as animal, s.date, s.details, s.position from tucha.state s' +
+        ' left join tucha.animal a on a.id = s.animal where s.is_deleted is null'
     }
 };
 
@@ -282,7 +355,7 @@ app.get('/r/adoptableAnimals', function (req, res) { // used in the slider, must
 
 // get grid
 app.get('/r/:entity', auth, function (req, res) {
-    query(selects[req.params.entity].details, res);
+    query(selects[req.params.entity].grid || selects[req.params.entity].details, res);
 });
 
 // get dropdown data
